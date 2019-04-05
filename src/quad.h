ec2e6f4d8fdc93db2e44e80ebc7416af26897887
Caitlin Bergervoet <cbe63@uclive.ac.nz>
2019-04-05 20:58:22 +1300

    Added some commenting

--- src/quad.h
+++ src/quad.h
@@ -3,12 +3,16 @@
 
 #include <stdint.h>
 
+//Initialise the quadrotor port/pins, and interrupt handler
 void initQaud(void);
 
+//Reset the value of the quadrotor slit counter variable to zero
 void resetQuad(void);
 
+//Converts the quadrotor slit counter to a value in degrees (of rotation of the helicopter)
 uint32_t getQuadAngle(void);
 
-void PortBIntHandler(void);
+//Iterates the quadrotor slit counter on an event, positively for CW, negatively for CCW
+void QuadIntHandler(void);
 
 #endif
