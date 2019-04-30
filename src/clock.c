273856357b6d4d47e5f8ac3b01767e9420d0a91e
nro55 <nro55@uclive.ac.nz>
2019-04-30 13:47:15 +1200

    Cleanup

--- src/clock.c
+++ src/clock.c
@@ -1,56 +1,17 @@
 #include "clock.h"
-#include "adc.h"
-#include "buttons4.h"
+#include "driverlib/sysctl.h"
 #include "inc/hw_memmap.h"
 #include "inc/hw_types.h"
-#include "driverlib/adc.h"
-#include "driverlib/systick.h"
 
 // Required by FreeRTOS
 uint32_t SystemCoreClock;
 
-//********************************************************
-// Initialisation functions: clock, SysTick, display, UART
-//********************************************************
+// Initialise system clock
 void initClock(void)
 {
     // Set the clock rate to 20 MHz
-    SysCtlClockSet(SYSCTL_SYSDIV_10 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
-                   SYSCTL_XTAL_16MHZ);
+    SysCtlClockSet(SYSCTL_SYSDIV_10 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
 
-	// Configure FreeRTOS clock speed 
+	// Configure FreeRTOS clock speed
 	SystemCoreClock = SysCtlClockGet();
-}
-
-//*******************************************************************
-void initSysTick(void)
-{
-    //
-    // Set up the period for the SysTick timer.  The SysTick timer period is
-    // set as a function of the system clock.
-    SysTickPeriodSet(SysCtlClockGet() / SAMPLE_RATE_HZ);
-    //
-    // Register the interrupt handler
-    SysTickIntRegister(SysTickIntHandler);
-    //
-    // Enable interrupt and device
-    SysTickIntEnable();
-    SysTickEnable();
-}
-
-//*****************************************************************************
-//
-// The interrupt handler for the for SysTick interrupt.
-//
-//*****************************************************************************
-void SysTickIntHandler(void)
-{
-    //
-    // Initiate a conversion
-    //
-    //ADCProcessorTrigger(ADC0_BASE, 3);
-    //g_ulSampCnt++;
-
-    // Poll buttons
-    updateButtons();
 }
--- src/clock.h
+++ src/clock.h
@@ -3,24 +3,13 @@
 
 #include <stdint.h>
 #include <stdbool.h>
-#include "driverlib/sysctl.h"
 
 #define SAMPLE_RATE_HZ 256
 
 // Required by FreeRTOS
 extern uint32_t SystemCoreClock;
 
-//********************************************************
-// Initialisation functions: clock, SysTick, display, UART
-//********************************************************
+// Initialise system clock
 void initClock(void);
-
-//*******************************************************************
-void initSysTick(void);
-
-//*****************************************************************************
-// The interrupt handler for the for SysTick interrupt.
-//*****************************************************************************
-void SysTickIntHandler(void);
 
 #endif
