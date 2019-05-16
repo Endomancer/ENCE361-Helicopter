99525194c6a63e7ac0b0de90514e752cb02ff900
Caitlin Bergervoet <cbe63@uclive.ac.nz>
2019-05-16 19:33:43 +1200

    Cleanup

--- src/controller.c
+++ src/controller.c
@@ -58,25 +58,4 @@ uint16_t control_update(pid_t* pid, int32_t pos)
     }
     
     return (uint32_t) control;
-// Function to update the control output depending on the current system errors and gains
-int32_t controlUpdate(controller_t *system)
-{
-    int32_t control = (system->Kp * system->p_error) + (system->Kd * (*system->d_error)) + (system->Ki * system->i_error);
-    return control;
-}
-
-
-// TODO Functions to update what i_error and d_error are?
-// Should these be included in controlUpdate?
-int32_t iErrorUpdate(controller_t *system)
-{
-    // adds the integral of the most recently written error in system->error
-    // to system->i_error
-    // Le: system->i_error += error * delta_t;
-}
-
-int32_t dErrorUpdate(controller_t *system)
-{
-    // Updates the differential error
-    // Le: d_error = (error - error_previous)/delta_t;
 }
