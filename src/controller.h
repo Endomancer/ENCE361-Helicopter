5320e60f669e40b096355dd5f42d887807360085
Narottam Royal <nro55@uclive.ac.nz>
2019-05-18 21:58:12 +1200

    Update declarations and add state machine enum

--- src/controller.h
+++ src/controller.h
@@ -3,6 +3,15 @@
 
 #include <stdint.h>
 
+// TODO: Yuck... but we don't have a better place to put it currently
+typedef enum
+{
+    LANDED,
+    SWEEPING,
+    FLYING,
+    LANDING
+} control_states_t;
+
 // PID structure
 typedef struct
 {
@@ -12,15 +21,18 @@ typedef struct
     int32_t p_error;    // Proportional error
     int32_t i_error;    // Integral error
     int32_t d_error;    // Derivative error
+    int32_t reference;  // Reference value
 } pid_t;
 
-pid_t initController(uint16_t Kp, uint16_t Ki, uint16_t Kd);
+// Initialise a PID controller instance
+void initController(pid_t* pid, uint16_t Kp, uint16_t Ki, uint16_t Kd);
 
+// Update the gains of a specified PID controller
+// Used for gain scheduling and experimentally finding gains
+void updateGains(pid_t* pid, uint16_t Kp, uint16_t Ki, uint16_t Kd);
 
-// *******************************************************
-// controlUpdate: Updates the control output based on the 
-// current system error and gains 
-uint16_t control_update(pid_t *pid, int32_t pos, uint32_t dT, int32_t desired);
+// Update the controller output based on the current system error and gains
+uint16_t controlUpdate(pid_t *pid, int32_t pos, uint32_t dT);
 
 
 #endif 
