a893a4a7ffcf529c528acc7718f8d982d6c31883
Narottam Royal <nro55@uclive.ac.nz>
2019-05-18 21:51:05 +1200

    Add getAltitude function

--- src/adc.h
+++ src/adc.h
@@ -20,6 +20,9 @@ void initADC(void);
 // Return the average value in the circular buffer
 uint32_t averageADCVal(void);
 
+// Return the average ADC value relative to the calibrated reference
+uint32_t getAltitude(void);
+
 // Calculate height as a percentage
 int32_t getHeight(void);
 
