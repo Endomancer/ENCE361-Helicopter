ce5c0f7e32739f30ac46f1d64dcbd83dc2143c06
Owen Plant <osp15@uclive.ac.nz>
2019-05-18 19:51:26 +1200

    Update calibration.c

--- src/calibration.c
+++ src/calibration.c
@@ -24,8 +24,8 @@ void sweepBooty()
     // turn on tail rotor at 40% duty
     enableTailRotor();
     enableMainRotor();
-    setMainRotorSpeed(25);
-    setTailRotorSpeed(25);
+    setMainRotorSpeed(30);
+    setTailRotorSpeed(30);
     // wait for the reference signal to turn high
     
     while (GPIOPinRead(GPIO_PORTC_BASE, GPIO_PIN_4) == GPIO_PIN_4);
