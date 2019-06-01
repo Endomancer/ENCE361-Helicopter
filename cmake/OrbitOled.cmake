af3d97f0ff9e8abe852e49f717c573bf8c87a76b
Narottam Royal <nro55@uclive.ac.nz>
2019-06-02 03:08:46 +1200

    Update filename

--- cmake/OrbitOled.cmake
+++
@@ -1,9 +0,0 @@
-include(FetchContent)
-
-FetchContent_Declare(
-  OrbitOLED
-  GIT_REPOSITORY https://eng-git.canterbury.ac.nz/nro55/tiva-libraries
-  GIT_TAG        OrbitOLED
-)
-
-FetchContent_MakeAvailable(OrbitOLED)
