6614d10044ac5ee8cd83347544b9d177436da901
Owen Plant <osp15@uclive.ac.nz>
2019-06-02 20:08:22 +1200

    Finished commenting

--- src/tasks.h
+++ src/tasks.h
@@ -1,6 +1,23 @@
 #ifndef TASKS_H
 #define TASKS_H
 
+// *******************************************************
+//
+// tasks.h
+//
+// Support for all tasks required for the project, these are:
+//      - ADC
+//      - Buttons
+//      - Calibration
+//      - Controller
+//      - OLEDDisplay
+//      - UART
+//
+// Edited for the helicopter project by Thu_am_group1
+// Last modified:  2.6.19
+//
+// *******************************************************
+
 #include "FreeRTOS.h"
 #include "task.h"
 
